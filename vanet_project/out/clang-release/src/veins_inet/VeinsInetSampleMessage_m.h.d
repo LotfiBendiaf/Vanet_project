4380b7006169e7715091b99a4faf856a0b92edf8
admaio <9966390+admaio@users.noreply.github.com>
2022-01-15 13:52:02 +0100

    Delete vanet_project/out/clang-release/src directory
    Better not to include generated files in git repos. Only source files should be added

--- vanet_project/out/clang-release/src/veins_inet/VeinsInetSampleMessage_m.h.d
+++
@@ -1,9 +0,0 @@
-veins_inet/VeinsInetSampleMessage_m.cc veins_inet/VeinsInetSampleMessage_m.h : \
-	veins_inet/VeinsInetSampleMessage.msg \
-	/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/INETDefs.msg \
-	/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/Units.msg \
-	/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/packet/chunk/Chunk.msg
-veins_inet/VeinsInetSampleMessage.msg:
-/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/INETDefs.msg:
-/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/Units.msg:
-/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/packet/chunk/Chunk.msg:
