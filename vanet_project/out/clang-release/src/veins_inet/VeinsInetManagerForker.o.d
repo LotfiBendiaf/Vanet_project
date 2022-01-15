32ad469d2de18c4e58ddb215039f2d37af8b36bd
Lotfi BENDIAF <55086608+LotfiBendiaf@users.noreply.github.com>
2022-01-15 05:20:32 +0100

    Initial_commit(Vanet_project)

---
+++ vanet_project/out/clang-release/src/veins_inet/VeinsInetManagerForker.o.d
@@ -0,0 +1,124 @@
+../out/clang-release/src/veins_inet/VeinsInetManagerForker.o: \
+  veins_inet/VeinsInetManagerForker.cc \
+  veins_inet/VeinsInetManagerForker.h veins_inet/veins_inet.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/veins.h \
+  /Users/lotfibendiaf/OMNet++/omnetpp-6.0pre15/include/omnetpp.h \
+  /Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/INETDefs.h \
+  /Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/precompiled_release.h \
+  /Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/precompiled.h \
+  /Users/lotfibendiaf/OMNet++/inet4.3/src/inet/opp_defines.h \
+  /Users/lotfibendiaf/OMNet++/inet4.3/src/inet/features.h \
+  /Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/Compat.h \
+  /Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/InitStages.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIScenarioManagerForker.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIScenarioManager.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/Coord.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/FWMath.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/modules/BaseWorldUtility.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/connectionManager/BaseConnectionManager.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/AntennaPosition.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/connectionManager/NicEntry.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/Heading.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/utility/HasLogProxy.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/FindModule.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/obstacle/ObstacleControl.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/obstacle/Obstacle.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/world/annotations/AnnotationManager.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/utility/BBoxLookup.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/obstacle/VehicleObstacleControl.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/Move.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/obstacle/MobileHostObstacle.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIBuffer.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConstants.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIColor.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConnection.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoord.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/VehicleSignal.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/EnumBitset.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCILauncher.h \
+  veins_inet/VeinsInetManagerBase.h \
+  /Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/utility/SignalManager.h
+
+veins_inet/VeinsInetManagerForker.h:
+
+veins_inet/veins_inet.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/veins.h:
+
+/Users/lotfibendiaf/OMNet++/omnetpp-6.0pre15/include/omnetpp.h:
+
+/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/INETDefs.h:
+
+/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/precompiled_release.h:
+
+/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/precompiled.h:
+
+/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/opp_defines.h:
+
+/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/features.h:
+
+/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/Compat.h:
+
+/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/InitStages.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIScenarioManagerForker.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIScenarioManager.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/Coord.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/FWMath.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/modules/BaseWorldUtility.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/connectionManager/BaseConnectionManager.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/AntennaPosition.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/connectionManager/NicEntry.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/Heading.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/utility/HasLogProxy.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/FindModule.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/obstacle/ObstacleControl.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/obstacle/Obstacle.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/world/annotations/AnnotationManager.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/utility/BBoxLookup.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/obstacle/VehicleObstacleControl.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/Move.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/obstacle/MobileHostObstacle.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIBuffer.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConstants.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIColor.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConnection.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoord.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/VehicleSignal.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/base/utils/EnumBitset.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/mobility/traci/TraCILauncher.h:
+
+veins_inet/VeinsInetManagerBase.h:
+
+/Users/lotfibendiaf/OMNet++/veins-veins-5.2/src/veins/modules/utility/SignalManager.h:
---
+++ vanet_project/out/clang-release/src/veins_inet/VeinsInetSampleMessage_m.h.d
@@ -0,0 +1,9 @@
+veins_inet/VeinsInetSampleMessage_m.cc veins_inet/VeinsInetSampleMessage_m.h : \
+	veins_inet/VeinsInetSampleMessage.msg \
+	/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/INETDefs.msg \
+	/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/Units.msg \
+	/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/packet/chunk/Chunk.msg
+veins_inet/VeinsInetSampleMessage.msg:
+/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/INETDefs.msg:
+/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/Units.msg:
+/Users/lotfibendiaf/OMNet++/inet4.3/src/inet/common/packet/chunk/Chunk.msg:
